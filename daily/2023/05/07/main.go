a748a5ed9e3710952b22f020ddae2164af6747cc
Robert <ilmself@hotmail.com>
2023-05-08 16:23:11 +0800

    feat: add 2023-05-07 daily code

--- daily/2023/05/07/main.go
+++ daily/2023/05/07/main.go
@@ -6,6 +6,8 @@ import (
 	"reflect"
 )
 
+// 判断两个 map 是否相现
+// 这个方法是判断给定的 dm1 是否是 dm2 的真子集的逻辑
 func CompareMap(dm1 map[string]interface{}, dm2 map[string]interface{}) bool {
 	keySlice := make([]string, 0)
 	data1Slice := make([]interface{}, 0)
@@ -26,7 +28,9 @@ func CompareMap(dm1 map[string]interface{}, dm2 map[string]interface{}) bool {
 	return string(data1Bytes) == string(data2Bytes)
 }
 
-// EqualMap 比较 map 是否相等（只适用成员为标量的map）
+// EqualMap 比较 map 是否相等
+// * 只适用成员为标量的 map
+// 这个方法是判断给定的 m1 是否是 m2 的真子集的逻辑
 func EqualMap(m1, m2 map[string]interface{}) bool {
 	if (0 == len(m1) && 0 == len(m2)) || (nil == m1 && nil == m2) {
 		return true
@@ -56,19 +60,52 @@ func EqualMapReflect(m1, m2 map[string]interface{}) bool {
 	return reflect.DeepEqual(m1, m2)
 }
 
+// 类型转换
+func convertType(in map[string]int) (out map[string]interface{}) {
+	out = make(map[string]interface{})
+	for k, v := range in {
+		out[k] = v
+	}
+	return
+}
+
 func main() {
 	var m map[string]int
 	var n map[string]int
+	fmt.Println("m is nil? ", m == nil)
+	fmt.Println("n is nil? ", n == nil)
 
-	fmt.Println(m == nil)
-	fmt.Println(n == nil)
+	m = make(map[string]int)
+	n = make(map[string]int)
+	m["a"] = 0
+	n["a"] = 0
+	n["b"] = 1
+
+	fmt.Println("m is nil? ", m == nil)
+	fmt.Println("n is nil? ", n == nil)
 
 	// 不能通过编译
 	//fmt.Println(m == n)
 
-	fmt.Println(reflect.DeepEqual(m, n))
+	c_m1 := convertType(m)
+	c_n1 := convertType(n)
+	fmt.Println("m[string]interface{} := ", c_m1)
+	fmt.Println("n[string]interface{} := ", c_n1)
 
-	cm1 := CompareMap((map[string]interface{})m, n)
-	fmt.Println(cm1)
+	// 使用反射中的方法判断
+	fmt.Println("内置的反射方法判断", reflect.DeepEqual(m, n))
+
+	cm1 := CompareMap(c_m1, c_n1)
+	fmt.Println("CompareMap 方法判断(1)", cm1)
+	cm1 = CompareMap(c_n1, c_m1)
+	fmt.Println("CompareMap 方法判断(2)", cm1)
+
+	cm2 := EqualMap(c_m1, c_n1)
+	fmt.Println("EqualMap 方法判断(1)", cm2)
+	cm2 = EqualMap(c_n1, c_m1)
+	fmt.Println("EqualMap 方法判断(2)", cm2)
+
+	cm3 := EqualMapReflect(c_m1, c_n1)
+	fmt.Println("EqualMapReflect 方法判断", cm3)
 
 }
