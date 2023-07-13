e9a9013e5104199af5631bac7a0ce4dbacb5c5c6
Robert <ilmself@hotmail.com>
2023-07-13 12:31:00 +0800

    feat: add 2023-07-13 daily and update index list

---
+++ daily/2023/07/13/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	age int
+}
+
+func main() {
+	person := &Person{28}
+
+	// 1.
+	defer fmt.Println(person.age)
+
+	// 2.
+	defer func(p *Person) {
+		fmt.Println(p.age)
+	}(person)
+
+	// 3.
+	defer func() {
+		fmt.Println(person.age)
+	}()
+
+	person.age = 29
+}
